965bf1a97220e8262964e9c85f902ba90ef888ac
Adrian <ad.dever@yahoo.com>
2023-10-08 18:32:26 +0200

    added logic

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const app = express();
 const uuid = require('uuid');
 const fs = require('fs');
 const path = require('path');
+const mongoose = require('mongoose');
+const Models = require('./models')
 
-app.use(bodyParser.json());
+const Movies = Models.Movie;
+const Users = Models.User;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/myflix', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Middleware to log requests
 app.use((req, res, next) => {
@@ -19,6 +28,11 @@ app.use((req, res, next) => {
   next();
 });
 
+// default text response when at /
+app.get('/', (req, res) => {
+  res.send('Welcome to myflix!');
+})
+
 // Serve the "documentation.html" file from the "public" folder
 app.get('/documentation.html', (req, res) => {
   const docPath = path.join(__dirname, 'public', 'documentation.html');
@@ -31,231 +45,106 @@ app.get('/documentation.html', (req, res) => {
   });
 });
 
-let users = [
-  {
-    id: 1,
-    name: 'Kim',
-    favoriteMovies: [],
-  },
-  {
-    id: 2,
-    name: 'Joe',
-    favoriteMovies: ['The Fountain'],
-  },
-];
-
-let movies = [
-  {
-    'Title': 'The Fountain',
-    'Description':
-      'As a modern day scientist, Tommy is struggling with mortality, desperately searching for the medical breakthrough that will save the life of his cancer-stricken wife, Izzi.',
-    'Genre': {
-      'Name': 'Drama',
-      'Description':
-        'In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.',
-    },
-    'Director': {
-      'Name': 'Darren Aronofsky',
-      'Bio': 'Darren bio ...',
-    },
-    'ImageURL':
-      'https://m.media-amazon.com/images/M/MV5BMTU5OTczMTcxMV5BMl5BanBnXkFtZTC´cwNDg3MTEzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg',
-    'Featured': false,
-  },
-  {
-    'Title': 'Requiem for a Dream',
-    'Description':
-      'A dark and intense drama that explores addiction and its consequences.',
-    'Genre': {
-      'Name': 'Drama',
-      'Description':
-        'In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.',
-    },
-    'Director': {
-      'Name': 'Darren Aronofsky',
-      'Bio': 'Darren bio ...',
-    },
-    'ImageURL': 'Link to the movie poster image',
-    'Featured': false,
-  },
-  {
-    'Title': 'Eternal Sunshine of the Spotless Mind',
-    'Description':
-      'Blends science fiction elements with a deep emotional story about memory and love.',
-    'Genre': {
-      'Name': 'Drama',
-      'Description':
-        'In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.',
-    },
-    'Director': {
-      'Name': 'Michel Gondry',
-      'Bio': 'Michel Gondry bio ...',
-    },
-    'ImageURL': 'Link to the movie poster image',
-    'Featured': false,
-  },
-  {
-    'Title': 'Black Swan',
-    'Description':
-      'A psychological thriller with elements of drama and horror, starring Natalie Portman as a ballet dancer.',
-    'Genre': {
-      'Name': 'Drama',
-      'Description':
-        'In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.',
-    },
-    'Director': {
-      'Name': 'Darren Aronofsky',
-      'Bio': 'Darren bio ...',
-    },
-    'ImageURL': 'Link to the movie poster image',
-    'Featured': false,
-  },
-  {
-    'Title': 'Tree of Life',
-    'Description':
-      'A visually stunning film that delves into themes of life, death, and the universe.',
-    'Genre': {
-      'Name': 'Drama',
-      'Description':
-        'In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.',
-    },
-    'Director': {
-      'Name': 'Terrence Malick',
-      'Bio': 'Terrence Malick bio ...',
-    },
-    'ImageURL': 'Link to the movie poster image',
-    'Featured': false,
-  },
-  {
-    'Title': 'Interstellar',
-    'Description': 'Explores space exploration, time, and the human spirit.',
-    'Genre': {
-      'Name': 'Drama',
-      'Description':
-        'In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.',
-    },
-    'Director': {
-      'Name': 'Christopher Nolan',
-      'Bio': 'Christopher Nolan bio ...',
-    },
-    'ImageURL': 'Link to the movie poster image',
-    'Featured': false,
-  },
-  {
-    'Title': 'Melancholia',
-    'Description':
-      'A psychological drama with a sci-fi twist, exploring the relationships of two sisters during an impending planetary collision.',
-    'Genre': {
-      'Name': 'Drama',
-      'Description':
-        'In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.',
-    },
-    'Director': {
-      'Name': 'Lars von Trier',
-      'Bio': 'Lars von Trier bio ...',
-    },
-    'ImageURL': 'Link to the movie poster image',
-    'Featured': false,
-  },
-  {
-    'Title': 'Sunshine',
-    'Description':
-      'A sci-fi drama about a group of astronauts sent to reignite the dying sun.',
-    'Genre': {
-      'Name': 'Drama',
-      'Description':
-        'In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.',
-    },
-    'Director': {
-      'Name': 'Danny Boyle',
-      'Bio': 'Danny Boyle bio ...',
-    },
-    'ImageURL': 'Link to the movie poster image',
-    'Featured': false,
-  },
-  {
-    'Title': 'The Lovely Bones',
-    'Description':
-      'Blends drama and elements of fantasy to tell the story of a young girl watching over her family from the afterlife.',
-    'Genre': {
-      'Name': 'Drama',
-      'Description':
-        'In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.',
-    },
-    'Director': {
-      'Name': 'Peter Jackson',
-      'Bio': 'Peter Jackson bio ...',
-    },
-    'ImageURL': 'Link to the movie poster image',
-    'Featured': false,
-  },
-  {
-    'Title': 'Another Earth',
-    'Description':
-      'An indie film that combines drama and science fiction, exploring the discovery of a parallel Earth.',
-    'Genre': {
-      'Name': 'Drama',
-      'Description':
-        'In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.',
-    },
-    'Director': {
-      'Name': 'Mike Cahill',
-      'Bio': 'Mike Cahill bio ...',
-    },
-    'ImageURL': 'Link to the movie poster image',
-    'Featured': false,
-  },
-  {
-    'Title': 'Moon',
-    'Description':
-      'A minimalist sci-fi drama that is a character study of a man working alone on a lunar base.',
-    'Genre': {
-      'Name': 'Drama',
-      'Description':
-        'In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.',
-    },
-    'Director': {
-      'Name': 'Duncan Jones',
-      'Bio': 'Duncan Jones bio ...',
-    },
-    'ImageURL': 'Link to the movie poster image',
-    'Featured': false,
-  },
-];
-
-// CREATE
-app.post('/users', (req, res) => {
-  const newUser = req.body;
-
-  if (newUser.name) {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).json(newUser);
-  } else {
-    res.status(400).send('users need names');
-  }
+// CREATE users
+app.post('/users', async (req, res) => {
+  await Users.findOne({ Name: req.body.Username })
+  .then((user) => {
+    if (user) {
+      return res.status(400).send(req.body.Username + 'already exists');
+    } else {
+      Users
+      .create({
+        Username: req.body.Name,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      })
+      .then((user) => {res.status(201).json(user)})
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status(500).send('Error ' + error);
+  });
 });
 
-// UPDATE
-app.put('/users/:id', (req, res) => {
-  const { id } = req.params;
-  const updatedUser = req.body;
 
-  let user = users.find((user) => user.id == id);
+// Get a user by username
+app.get('/users/:Username', async (req, res) => {
+  const Username = req.params.Username;
+  console.log('Searching for user with Username:', Username);
 
-  if (user) {
-    user.name = updatedUser.name;
-    res.status(200).json(user);
-  } else {
-    res.status(400).send('no such user');
-  }
+  await Users.findOne({ Username: Username })
+    .then((user) => {
+      if (user) {
+        res.json(user);
+      } else {
+        console.log('User not found:', Username);
+        res.status(404).send('User not found');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error ' + err);
+    });
 });
+
+// Get all users
+app.get('/users', async (req, res) => {
+  await Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error ' + err);
+    });
+});
+
+// Delete a user
+app.delete('/users/:Username', async (req, res) => {
+  const Username = req.params.Username;
+  await Users.findOneAndRemove({ Username: Username })
+    .then((user) => {
+      if (!user) {
+        res.status(404).send(Username + ' was not found');
+      } else {
+        res.status(200).send(Username + ' was deleted');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error ' + err);
+    });
+});
+
+
+
+// Add a movie to a user's list of favorites
+app.post('/users/:Username/movies/:MovieID', async (req, res) => {
+  await Users.findOneAndUpdate({ Username: req.params.Username },
+    { $push: { FavoriteMovies: req.params.MovieID }
+  },
+  { new: true }) // This line makes sure the updated document is returned
+  then((updatedUser) => {
+    res.json(updatedUser);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error ' + err);
+  });
+});
+
 
 // UPDATE
 app.post('/users/:id/:movieTitle', (req, res) => {
   const { id, movieTitle } = req.params;
 
-  let user = users.find((user) => user.id == id);
+  let user = Users.find((user) => user.id == id);
 
   if (user) {
     user.favoriteMovies.push(movieTitle);
@@ -269,7 +158,7 @@ app.post('/users/:id/:movieTitle', (req, res) => {
 app.delete('/users/:id/:movieTitle', (req, res) => {
   const { id, movieTitle } = req.params;
 
-  let user = users.find((user) => user.id == id);
+  let user = Users.find((user) => user.id == id);
 
   if (user) {
     user.favoriteMovies = user.favoriteMovies.filter(
@@ -297,47 +186,59 @@ app.delete('/users/:id', (req, res) => {
   }
 });
 
-// READ
+// return JSON object when at /movies
 app.get('/movies', (req, res) => {
-  res.status(200).json(movies);
+  Movies.find()
+  .then((movies) => {
+    res.status(201).json(movies);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error ' + err);
+  });
 });
 
-// READ
+// READ movie titles
 app.get('/movies/:title', (req, res) => {
-  const { title } = req.params;
-  const movie = movies.find((movie) => movie.Title === title);
-
-  if (movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(400).send('no such movie');
-  }
+  Movies.findOne({ Title: req.params.Title })
+  .then((movie) => {
+    res.json(movie);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error ' + err);
+  });
 });
 
-// READ
-app.get('/movies/genre/:genreName', (req, res) => {
-  const { genreName } = req.params;
-  const genre = movies.find((movie) => movie.Genre.Name === genreName).Genre;
 
-  if (genre) {
-    res.status(200).json(genre);
-  } else {
-    res.status(400).send('no such genre');
-  }
+// READ genre
+app.get('/genre/:Name', (req, res) => {
+  Genres.findOne({ Name: req.params.Name })
+    .then((genre) => {
+      if (!genre) {
+        // Handle the case when the genre is not found.
+        res.status(404).send('Genre not found');
+      } else {
+        res.json(genre.Description);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error ' + err);
+    });
 });
 
-// READ
-app.get('/movies/directors/:directorName', (req, res) => {
-  const { directorName } = req.params;
-  const director = movies.find(
-    (movie) => movie.Director.Name === directorName
-  ).Director;
 
-  if (director) {
-    res.status(200).json(director);
-  } else {
-    res.status(400).send('no such director');
-  }
+// READ
+app.get('/director/:Name', (req, res) => {
+  Directors.findOne({ Name: req.params.Name})
+  .then((director) => {
+    res.json(director);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error ' + err);
+  });
 });
 
 app.listen(8080, () => console.log('listening on 8080'));
